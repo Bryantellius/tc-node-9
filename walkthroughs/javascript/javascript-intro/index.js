4a081d5d78f7c739075dbb6490caed5cd766c88f
Ben <brbryant2639@gmail.com>
2022-08-11 13:49:00 -0500

    UPDATE: added js intro

---
+++ walkthroughs/javascript/javascript-intro/index.js
@@ -0,0 +1,23 @@
+// Initializing variables (Declaration + Assignment)
+let name = "Blue Mic"; // mic name is Blue Mic
+let volume = 20; // level is at 20
+let isMuted = false; // not muted
+var roomName = "Podcast room";
+const id = "1234";
+
+console.log("Hello World!");
+
+console.log("name:", name);
+console.log("volume:", volume);
+console.log("isMuted:", isMuted);
+console.log(roomName);
+console.log(id);
+
+// Reassigning variables
+name = "Red Mic";
+volume = 30;
+isMuted = true;
+
+console.log("name:", name);
+console.log("volume:", volume);
+console.log("isMuted:", isMuted);
