979d2d1e69939c115f2d6247d3e6cb303e2d8f64
Ben <brbryant2639@gmail.com>
2022-08-12 17:42:10 -0500

    UPDATE: added js control flow

---
+++ reviews/javascript.md
@@ -0,0 +1,60 @@
+# JavaScript
+
+## What is JavaScript?
+
+- Not Java
+- An object-oriented programming language primarily used to create dynamic web interfaces
+- Multi paradigm programming language
+  - Functional
+  - Procedural
+  - Object-Oriented
+
+## What is a variable?
+
+- A container for a value
+- Variable names called _identifiers_
+- Use variable names to reference/use the value stored in a variable
+- Use _declaration keywords_ to create a variable
+  - `var`, can be reassigned
+  - `let`, can be reassigned
+  - `const`, cannot be reassigned
+
+## Declaration and Assignment
+
+To declare a variable:
+
+```js
+let name;
+```
+
+To assign a variable a value:
+
+```js
+name = "Ben";
+```
+
+Declaring and assigning a variable with a value is called _Initialization_.
+
+```js
+let name = "Ben";
+```
+
+## What are the JavaScript data types?
+
+- Primitive Data types
+  1. String: "Anything in quotes", 'Anything in single quotes', `Anything in backticks`
+  2. Number: 12, 3.14, 3e-14
+  3. Boolean: true, false
+  4. Undefined: undefined
+  5. Null: null
+  6. Symbol: Symbol("unique")
+  7. BigInt: 1n, 0n, 123456788901234567890n
+- Structured Data types
+
+## What are operators in JavaScript?
+
+- Assignment, `=`
+- Arithmetic, `+, -, *, /, **, %`
+  - Concatenation, `+`
+- Comparison, `==, ===, !=, !==, >, >=, <, <=`
+- Logical, `&&, ||, !`
--- walkthroughs/javascript/javascript-operators/index.js
+++ walkthroughs/javascript/javascript-operators/index.js
@@ -1,14 +1,53 @@
 console.log("Hello World");
 
-let daysInYear = 365.25;
+// Exercise 1
 
-let dogYears = 7;
+let a = 20;
+let b = 4;
+let c = 10;
+let d = 25;
 
-// PEMDAS
-let dogDays = dogYears * daysInYear;
+let addition = a + b; // -> 24
+let subtraction = a - b; // -> 16
+let multiplication = a * b; // -> 80
+let division = a / b; // -> 5
 
-console.log(dogDays);
+let remainder = d % c; // -> 5
+let power = c ** b; // -> 10,000
 
-console.log("One dog year is " + dogDays + " dog days.");
+let special = (((a + b) * (b - c)) / c ** d) % true;
 
-console.log("Dog days is between 0 and 3000:", 3000 > dogDays && dogDays > 0);
+console.log(addition, subtraction, multiplication, division, remainder, power);
+
+// Exercise 2
+
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+console.log(num + str); // -> "1111"
+console.log(num + str2); // -> "11eleven"
+console.log(num + isPresent); // -> 12
+console.log(firstName + num); // -> "Frodo11"
+console.log(isPresent + str); // -> "true11"
+console.log(firstName + lastName); // -> "FrodoBaggins"
+
+// Exercise 3
+
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isAwake = false;
+
+console.log(val == str3); // -> true
+console.log(val === str3); // -> false
+console.log(!isAwake); // -> true
+console.log("eleven" == str4 && val >= str3); // -> false
+console.log(!isAwake || isAwake); // -> true
+console.log(0 == false); // -> true
+console.log(0 === false); // -> false
+console.log(0 != false); // -> false
+console.log(0 !== false); // -> true
