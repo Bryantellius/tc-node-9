e8195e2821d6951a2a7f124419263c5ddb72a346
Ben <brbryant2639@gmail.com>
2022-08-15 14:56:47 -0500

    UPDATE: added js loops lesson

--- walkthroughs/javascript/javascript-loops/lesson.js
+++ walkthroughs/javascript/javascript-loops/lesson.js
@@ -12,3 +12,35 @@ for (let num = 1; num <= 10; num++) {
 }
 
 console.log(`The average number between 1 and 10 is: ${sum / count}`);
+
+for (let num = 1; num <= 10; num++) {
+  console.log(num);
+}
+
+let num = 1;
+
+while (num <= 10) {
+  // loop body
+  console.log(num);
+  num++;
+}
+
+// as a do/while
+
+let x = 11;
+
+do {
+  console.log(x);
+  x++;
+} while (x <= 10);
+
+let min = 1;
+let max = 12;
+
+for (let num = min; num <= max; num++) {
+  for (let multiplier = min; multiplier <= max; multiplier++) {
+    console.log(`${num} x ${multiplier} = ${num * multiplier}`);
+  }
+}
+
+num % 2 == 0; // even
