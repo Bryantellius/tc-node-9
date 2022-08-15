ae0b6729f26068fffdfedc61e38a55506a3e7987
Ben <brbryant2639@gmail.com>
2022-08-15 15:23:09 -0500

    UPDATE: added exercise start

--- walkthroughs/javascript/javascript-loops/exercise.js
+++ walkthroughs/javascript/javascript-loops/exercise.js
@@ -1,1 +1,29 @@
 // TODO
+
+// iterate from 1 to 100
+// print odds
+for (let i = 1; i <= 100; i++) {
+  // evaluate odds
+  // print
+}
+
+// iterate from 1 to 100
+// print odds
+
+let i = 1;
+
+while (i <= 100) {
+  // evaluate odds
+  // print
+
+  i++;
+}
+
+let x = 1;
+
+do {
+  // evaluate odds
+  // print
+
+  x++;
+} while (x <= 100);
