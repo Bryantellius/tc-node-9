cea8efb19e6d7399d171840ff981fafcb61f4d41
Ben <brbryant2639@gmail.com>
2022-08-16 09:59:41 -0500

    UPDATE: started js loops exercise

--- walkthroughs/javascript/javascript-loops/exercise.js
+++ walkthroughs/javascript/javascript-loops/exercise.js
@@ -1,29 +1,102 @@
-// TODO
+// Exercise One
+console.log("IGNORE EVENS: FOR");
 
-// iterate from 1 to 100
-// print odds
-for (let i = 1; i <= 100; i++) {
-  // evaluate odds
-  // print
+let start = 0;
+let end = 15;
+
+for (let i = start; i <= end; i++) {
+  if (i % 2 == 1) {
+    console.log(i, "odd");
+  }
 }
 
-// iterate from 1 to 100
-// print odds
+// while rewrite
+console.log("IGNORE EVENS: WHILE");
 
-let i = 1;
+let whileI = 0;
 
-while (i <= 100) {
-  // evaluate odds
-  // print
+while (whileI <= end) {
+  if (whileI % 2 == 1) {
+    console.log(whileI, "odd");
+  }
 
-  i++;
+  whileI++;
 }
 
-let x = 1;
+// do/while rewrite
+console.log("IGNORE EVENS: DO/WHILE");
+
+let doWhileI = 0;
 
 do {
-  // evaluate odds
-  // print
+  if (doWhileI % 2 == 1) {
+    console.log(doWhileI, "odd");
+  }
 
-  x++;
-} while (x <= 100);
+  doWhileI++;
+} while (doWhileI <= end);
+
+// Exercise Two: FIZZBUZZ
+console.log("FIZZBUZZ: FOR");
+
+for (let i = start; i <= end; i++) {
+  let output = "";
+
+  if (i % 3 == 0) {
+    // FIZZ
+    output += "FIZZ";
+  }
+
+  if (i % 5 == 0) {
+    // BUZZ
+    output += "BUZZ";
+  }
+
+  output && console.log(i, output);
+}
+
+// while rewrite
+console.log("FIZZBUZZ: while");
+
+let whileF = start;
+
+while (whileF <= end) {
+  let output = "";
+
+  if (whileF % 3 == 0) {
+    // FIZZ
+    output += "FIZZ";
+  }
+
+  if (whileF % 5 == 0) {
+    // BUZZ
+    output += "BUZZ";
+  }
+
+  output && console.log(whileF, output);
+
+  whileF++;
+}
+
+// do/while rewrite
+console.log("FIZZBUZZ: do/while");
+
+let doWhileF = start;
+
+do {
+  let output = "";
+
+  if (doWhileF % 3 == 0) {
+    // FIZZ
+    output += "FIZZ";
+  }
+
+  if (doWhileF % 5 == 0) {
+    // BUZZ
+    output += "BUZZ";
+  }
+
+  output && console.log(doWhileF, output);
+
+  doWhileF++;
+} while (doWhileF <= end);
