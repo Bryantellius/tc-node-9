290e83292431e1a24798975e0b7a1fef4b7fe022
Ben <brbryant2639@gmail.com>
2022-08-18 12:00:28 -0500

    UPDATE: quiz game example with ood

--- walkthroughs/javascript/javascript-objects/exercise.js
+++ walkthroughs/javascript/javascript-objects/exercise.js
@@ -1,1 +1,39 @@
 console.log("Hello Objects");
+
+// Quiz App
+// questions
+// score
+
+let game = {
+  score: 0,
+  questions: [
+    {
+      text: "What is the JavaScript keyword for creating a variable with a constant value?",
+      answer: "const",
+    },
+    {
+      text: "What is the JavaScript keyword for creating a block-scoped variable that can be re-assigned?",
+      answer: "let",
+    },
+    {
+      text: "What is the JavaScript keyword for creating a function-scoped variable that can be re-assigned?",
+      answer: "var",
+    },
+  ],
+  start: function () {
+    this.questions.forEach((question) => {
+      this.askQuestion(question);
+    });
+
+    alert(`You got ${this.score} of ${this.questions.length}.`);
+  },
+  askQuestion: function (question) {
+    let answer = prompt(question.text);
+
+    if (answer == question.answer) {
+      this.score++;
+    }
+  },
+};
+
+game.start();
