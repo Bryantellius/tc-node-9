288da3065190ea294a17db98ca39f3d1152c9c2d
Ben <brbryant2639@gmail.com>
2022-08-18 15:08:00 -0500

    UPDATE: js objects exercise

--- walkthroughs/javascript/javascript-objects/exercise.js
+++ walkthroughs/javascript/javascript-objects/exercise.js
@@ -1,39 +1,149 @@
 console.log("Hello Objects");
 
-// Quiz App
-// questions
-// score
+const numbers = [2, 22, 12, 17, 18, 39, 129];
 
-let game = {
-  score: 0,
-  questions: [
-    {
-      text: "What is the JavaScript keyword for creating a variable with a constant value?",
-      answer: "const",
-    },
-    {
-      text: "What is the JavaScript keyword for creating a block-scoped variable that can be re-assigned?",
-      answer: "let",
-    },
-    {
-      text: "What is the JavaScript keyword for creating a function-scoped variable that can be re-assigned?",
-      answer: "var",
-    },
-  ],
-  start: function () {
-    this.questions.forEach((question) => {
-      this.askQuestion(question);
-    });
+function arraySum(arr) {
+  let sum = 0;
 
-    alert(`You got ${this.score} of ${this.questions.length}.`);
-  },
-  askQuestion: function (question) {
-    let answer = prompt(question.text);
+  // find sum
+  //   for (let i = 0; i < arr.length; i++) {
+  //     sum += arr[i];
+  //   }
+  // function updateSum(num) {
+  //     sum += num;
+  // }
 
-    if (answer == question.answer) {
-      this.score++;
-    }
+  // arr.forEach(updateSum)
+  arr.forEach((value) => {
+    sum += value;
+  });
+
+  return sum;
+}
+
+let sum1 = arraySum(numbers);
+let sum2 = arraySum([1, 2, 3, 4, 5]); // 15
+
+console.log(sum1, sum2);
+
+let a = 2;
+
+a += 5; // a = a + 5
+
+const add = (a, b) => {
+  return a + b;
+};
+
+function printSquared(num) {
+  console.log(num ** 2);
+}
+
+function iterate(arr, func) {
+  for (let i = 0; i < arr.length; i++) {
+    func(arr[i]);
+  }
+}
+
+iterate(numbers, printSquared);
+
+iterate([1, 2, 3], function (num) {
+  console.log(num + " value");
+});
+
+function forEach(func) {
+  for (let i = 0; i < numbers.length; i++) {
+    let value = numbers[i];
+    let index = i;
+    let array = numbers;
+
+    func(value, index, array);
+  }
+}
+
+forEach(console.log);
+forEach((val) => {
+  console.log(`Iterating over ${val}`);
+});
+
+// Exercise 2
+
+let book = {
+  title: "Name of The Wind",
+  author: "Patrick Rothfuss",
+  pages: 722,
+  readCount: 3,
+  info: function () {
+    return `${this.title} by ${this.author} is ${this.pages} page(s) long, and has been read ${this.readCount} time(s).`;
   },
 };
 
-game.start();
+book.author; // "Patrick Rothfuss"
+
+book.readCount++;
+
+console.log(book.info());
+
+// Exercise 3
+
+let sentence = "The quick brown fox jumps over the lazy dog";
+
+// function to reverse a string
+function reverseStr(str) {
+  let reversedStr = "";
+
+  for (let i = 0; i < str.length; i++) {
+    reversedStr = str[i] + reversedStr;
+  }
+
+  return reversedStr;
+}
+
+function reverseWordsFromSentence(sentence) {
+  // split a sentence into words
+  let words = sentence.split(" ");
+  // reverse each word in a array
+  let reversedWords = words.map(function (word) {
+    return reverseStr(word);
+  });
+
+  return reversedWords.join(" ");
+}
+
+console.log(reverseWordsFromSentence(sentence));
+
+// Exercise 4
+
+let csvData = "name,age\nFrodo,50\nSam,38\nMerry,36\nPippin,26";
+
+console.log(csvData);
+
+function toJSON() {
+  let dataArr = csvData.split("\n");
+
+  // get the headers
+  let headers = dataArr[0];
+
+  // get each record
+  let records = dataArr.slice(1);
+
+  console.log(headers, records);
+}
+
+toJSON(csvData);
+
+let names = ["Ben"];
+
+// add elements to the back of an array
+names.push("Cameron");
+
+// remove elements to the back of an array
+names.pop();
+
+// add elements to the front of an array
+names.unshift("Seth");
+
+// remove elements to the front of an array
+names.shift();
+
+// adds or remove elements anywhere specified in an array
+names.splice(1, 1);
--- walkthroughs/javascript/javascript-objects/lesson.js
+++ walkthroughs/javascript/javascript-objects/lesson.js
@@ -195,3 +195,41 @@ let test1 = reverseStr("Hello World");
 let test2 = reverseStr("Ben Bryant");
 
 console.log(test1, test2);
+
+// Quiz App
+// questions
+// score
+
+let game = {
+  score: 0,
+  questions: [
+    {
+      text: "What is the JavaScript keyword for creating a variable with a constant value?",
+      answer: "const",
+    },
+    {
+      text: "What is the JavaScript keyword for creating a block-scoped variable that can be re-assigned?",
+      answer: "let",
+    },
+    {
+      text: "What is the JavaScript keyword for creating a function-scoped variable that can be re-assigned?",
+      answer: "var",
+    },
+  ],
+  start: function () {
+    this.questions.forEach((question) => {
+      this.askQuestion(question);
+    });
+
+    alert(`You got ${this.score} of ${this.questions.length}.`);
+  },
+  askQuestion: function (question) {
+    let answer = prompt(question.text);
+
+    if (answer == question.answer) {
+      this.score++;
+    }
+  },
+};
+
+game.start();
